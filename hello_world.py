c46b3f3b42fed6cad7a0165b46f8d08b7a65ce91
Alejandro Piad Morffis <apiad@apiad.net>
2021-06-30 21:21:25 -0400

    Deployed

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,1 @@
-# What is the meaning of life, the Universe, and everything?
-
-def answer():
     return None
--- test_solution.py
+++ test_solution.py
@@ -1,4 +1,1 @@
-from hello_world import answer
-
-def test_solution():
     assert answer() == 42
